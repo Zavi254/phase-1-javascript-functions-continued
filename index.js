287257bfa62e2744b2da57deac0a97f613e78e5e
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-07 23:35:03 +0300

    commit tree 0b78acc22544136b99162bf0d6cfaaccbde13ff3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(roller = `roller-skate`){
+    return `This Saturday, I want to ${roller}!`;
+}
+saturdayFun(`bathe my dog`);
+
+const mondayWork = function(whatToBeDone = `go to the office`){
+    return `This Monday, I will ${whatToBeDone}.`
+}
+mondayWork(`work from home`);
+
+
+function wrapAdjective(defaultOne = "*"){
+
+    function innerFunction(word = "special"){
+        return `You are ${defaultOne}${word}${defaultOne}!`
+    }
+
+    return innerFunction;
+}
+
+const final = wrapAdjective("%")("a dedicated programmer");
+
